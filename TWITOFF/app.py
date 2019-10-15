c8f9e52fdd33ca899b1e289b39a0a40fee408924
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-15 14:06:42 -0700

    add app config

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -2,11 +2,18 @@
 
 # Import important functions
 from flask import Flask
+from .models import DB
+import os
 
 
 def create_app():
     # create and comfigure a flask app instance
     app = Flask(__name__)
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+        basedir, 'data.sqlite')
+    DB.init_app(app)
 
     @app.route('/')
     def root():
