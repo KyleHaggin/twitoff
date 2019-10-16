637dda7d44612a94e3cf1d42ad0d99745c9e2d8d
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-16 12:41:47 -0700

    Update to begin twitter api functionality

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -8,11 +8,19 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     # Twitter users that we pull and anaylze
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 
 class Tweet(DB.Model):
     # Tweets
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('User'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
